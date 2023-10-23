094f8c94cabf0d8e5d06a00ee0bd23265f12c7a6
faizftw <faizfatwa030206@gmail.com>
2023-10-24 02:06:22 +0800

    page checkout dan metode pembayaran

--- .venv/main.py
+++ .venv/main.py
@@ -10,6 +10,14 @@ def index():
 def about():
     return render_template("indexi.html")
 
+@app.route("/Checkout")
+def Checkout():
+    return render_template("Checkout.html")
+
+@app.route("/Payment_Method")
+def Payment():
+    return render_template("Payment.html")
+
 @app.route("/contact")
 def contact():
     return "<p>Contact Us</p>"
