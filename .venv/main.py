16bcd69f524f3ee5a4679d9f5acd22a7dba7dc8c
Mhmmdfhkri <mufik2002@gmail.com>
2023-10-25 10:24:08 +0800

    perbaiki

--- .venv/main.py
+++ .venv/main.py
@@ -2,21 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
     return "<p>Hello, World!</p>"
-
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-
 @app.route("/keranjang")
 def keranjang():
     return render_template("keranjang.html")
-
 
 @app.route("/Checkout")
 def Checkout():
@@ -26,9 +22,17 @@ def Checkout():
 def Payment():
     return render_template("Payment.html")
 
-@app.route("/about")
-def about():
+@app.route("/first")
+def fist():
     return render_template("first.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/daftar")
+def register():
+    return render_template("login.html")
 
 @app.route("/contact")
 def contact():
