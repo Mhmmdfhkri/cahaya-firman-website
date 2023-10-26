d33a4eac1992ac4b0dd20d210ca3110f1578c0e2
faizftw <faizfatwa030206@gmail.com>
2023-10-26 20:36:12 +0800

    add user database for register

--- .venv/main.py
+++ .venv/main.py
@@ -1,12 +1,83 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin,login_user,LoginManager,login_required,logout_user,current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import InputRequired,Length,ValidationError,EqualTo, Email
+from flask_bcrypt import Bcrypt 
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'Cahayasyarif'
+db = SQLAlchemy(app)
+app.app_context().push()
+bcrypt = Bcrypt(app)
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique = True)
+    email = db.Column(db.String(120), nullable=False, unique=True) 
+    password = db.Column(db.String(80), nullable=False)
+    
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+        InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    email = StringField(validators=[
+        InputRequired(), Email()], render_kw={"placeholder": "Email Address"})
+
+    password = PasswordField(validators=[
+        InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Create Password"})
+
+    confirm_password = PasswordField(validators=[
+        InputRequired(), EqualTo('password', message='Passwords must match')], render_kw={"placeholder": "Confirm Password"})
+
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
+
+    def validate_email(self, email):
+        existing_user_email = User.query.filter_by(email=email.data).first()
+        if existing_user_email:
+            raise ValidationError(
+                'An account with this email already exists.')
+
+        
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
+
+
 @app.route("/")
 def index():
-    return "<p>Hello, World!</p>"
+    return render_template("first.html")
 
 @app.route("/home")
+@login_required
 def home():
     return render_template("home.html")
 @app.route("/about")
@@ -25,18 +96,33 @@ def Checkout():
 def Payment():
     return render_template("Payment.html")
 
-@app.route("/first")
-def fist():
-    return render_template("first.html")
-
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('home'))
+    return render_template("login.html",form=form)
 
-@app.route("/daftar")
+@app.route("/daftar", methods=['GET', 'POST'])
 def register():
-    return render_template("daftar.html")
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password,email=form.email.data)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template("daftar.html",form=form)
 
 @app.route("/contact")
 def contact():
     return "<p>Contact Us</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
