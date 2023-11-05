16e46c5f16963b3a859c6a2d6bdfbd4b52f56789
Mhmmdfhkri <mufik2002@gmail.com>
2023-11-06 07:07:08 +0800

    update css for product

--- app.py
+++ app.py
@@ -14,9 +14,21 @@ from models.product import Barang
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route("/")
-def index():
-    return render_template("first.html")
+# @app.route("/")
+# def index():
+#     return render_template("first.html")
+@app.route("/", methods=["GET", "POST"])
+def products():
+    if request.method == "POST":
+        # Handle adding to cart or purchasing the item here
+        # You can access the product's ID from the request
+        product_id = request.form.get("product_id")
+        # Logic to add the product to the cart or process the purchase
+        flash("Product added to cart successfully")
+        return redirect(url_for("products"))
+
+    barang_list = Barang.query.all()
+    return render_template("product.html", barang_list=barang_list)
 
 
 @app.route("/home")
@@ -72,19 +84,6 @@ def register():
 
     return render_template("daftar.html", form=form)
 
-
-@app.route("/products", methods=["GET", "POST"])
-def products():
-    if request.method == "POST":
-        # Handle adding to cart or purchasing the item here
-        # You can access the product's ID from the request
-        product_id = request.form.get("product_id")
-        # Logic to add the product to the cart or process the purchase
-        flash("Product added to cart successfully")
-        return redirect(url_for("products"))
-
-    barang_list = Barang.query.all()
-    return render_template("product.html", barang_list=barang_list)
 
 # admin start
 @app.route("/admin_user")
