60b64dec509cc4bca65e2733a0fb0a1b47ccfb6e
faizftw <faizfatwa030206@gmail.com>
2023-11-18 05:33:34 +0800

    add payment date column

--- models/payment_detail.py
+++ models/payment_detail.py
@@ -1,7 +1,9 @@
 from main import db
+from datetime import datetime
 
 class Payment_detail(db.Model):
     __tablename__ = 'payment_detail'  
     id_payment = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Integer) 
-    payment_method = db.Column(db.String(100)) 
+    payment_method = db.Column(db.String(100))
+    payment_date = db.Column(db.DateTime, default=datetime.utcnow) 
