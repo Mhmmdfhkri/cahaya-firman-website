448af973b17ccb30a9e9588f2b9bf408edb2d7a6
faizftw <faizfatwa030206@gmail.com>
2023-12-18 15:44:34 +0800

    delete column total from shopping session

--- models/shopping_session.py
+++ models/shopping_session.py
@@ -6,7 +6,6 @@ from sqlalchemy.orm import relationship
 class session(db.Model):
     id_session = db.Column(db.Integer, primary_key=True)
     id_user = db.Column(db.Integer, db.ForeignKey('user.id_user'), nullable=False)
-    total = db.Column(db.Float, nullable=False, default=0)
     is_active = db.Column(db.Boolean, default=True)
 
 
